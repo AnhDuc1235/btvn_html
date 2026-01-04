14ce54a2d60e0734b13f398406a791300af00e76
AnhDuc1235 <anhducdanghoang@gmail.com>
2026-01-04 18:55:34 +0700

    bai tap buoi 38

--- baitap-reactjs/buoi38/src/App.jsx
+++ baitap-reactjs/buoi38/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
-import AuthModal from './components/authModal'
+import AuthModal from './components/AuthModal'
 
 export default function App() {
   return (
